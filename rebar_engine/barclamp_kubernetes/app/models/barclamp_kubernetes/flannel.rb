5568dee1ed870e13ca47d140bf4354c2d54dc3d3
Greg Althaus <galthaus@austin.rr.com>
2015-12-09 14:20:35 -0600

    Trying to look at a node too early should crash the node view.

--- rebar_engine/barclamp_kubernetes/app/models/barclamp_kubernetes/flannel.rb
+++ rebar_engine/barclamp_kubernetes/app/models/barclamp_kubernetes/flannel.rb
@@ -7,7 +7,7 @@ class BarclampKubernetes::Flannel < Role
   def sysdata(nr)
     # GREG: Use admin address for now.
     {"flannel" => {
-        "address" => IP.coerce(nr.node.addresses(:v4_only).first).addr
+        "address" => (IP.coerce(nr.node.addresses(:v4_only).first).addr rescue "0.0.0.0")
       }
     }
   end
