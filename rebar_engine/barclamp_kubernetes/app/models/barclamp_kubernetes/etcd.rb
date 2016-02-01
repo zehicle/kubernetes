d59f00661568bd5bb39b4507dc7634bbab0f8e19
Greg Althaus <galthaus@austin.rr.com>
2016-02-01 12:03:58 -0600

    Put back the custom ruby roles so that we can make sure that deploy runs last of all the marker roles without using the cluster that is cluster.

---
+++ rebar_engine/barclamp_kubernetes/app/models/barclamp_kubernetes/etcd.rb
@@ -0,0 +1,17 @@
+#
+# Copyright 2016, RackN
+#
+
+class BarclampKubernetes::Etcd < Role
+
+  # Make sure the that new node role has deploy as a child.
+  def on_node_bind(nr)
+    r = Role.find_by_name('kubernetes-deploy')
+    items = NodeRole.peers_by_role(nr.deployment, r)
+    items.each do |item|
+        nr.add_child(item)
+    end
+  end
+
+end
+
